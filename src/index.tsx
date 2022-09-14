3dd0a8ddc324627ea73472056536e6b916040671
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-09-14 19:45:01 +0500

    fix: hash router in gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
